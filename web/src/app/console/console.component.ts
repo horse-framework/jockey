382f6322ea3ba6219618f2bc6e8032523d27dc03
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2021-04-09 01:48:18 +0300

    console design

--- web/src/app/console/console.component.ts
+++ web/src/app/console/console.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-console',
-  templateUrl: './console.component.html',
-  styleUrls: ['./console.component.css']
+    selector: 'app-console',
+    templateUrl: './console.component.html',
+    styleUrls: ['./console.component.css']
 })
 export class ConsoleComponent implements OnInit {
 
-  constructor() { }
+    source: string = 'queue';
+    targetType: string = 'name';
+    target: string = '';
 
-  ngOnInit(): void {
-  }
+    showTime: boolean = true;
+    showMessageId: boolean = false;
+    showContentType: boolean = false;
+
+    html: string = 'Data is created successfully';
+    //html: string = '{"name": "foo", "number": 123, "enabled": true, "other": "test"}';
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
