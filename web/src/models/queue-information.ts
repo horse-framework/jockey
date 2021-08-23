676eb041b076c8cf9ad8ac9a013ae19e0cd7375d
Emre Hızlı <hizliemre@gmail.com>
2021-08-23 18:29:08 +0300

    fix 6

--- web/src/models/queue-information.ts
+++ web/src/models/queue-information.ts
@@ -8,7 +8,6 @@ export interface QueueInformation {
     acknowledge: string;
     ackTimeout: number;
     msgTimeout: number;
-    useMessageId: boolean;
     messageLimit: number;
     messageSizeLimit: number;
     delayBetweenMessages: number;
