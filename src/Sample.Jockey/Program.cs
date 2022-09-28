bd8423bb8c5f68c3735829127dd7bac8f88820fe
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2022-09-28 10:43:59 +0300

    dashboard widget design fix

--- src/Sample.Jockey/Program.cs
+++ src/Sample.Jockey/Program.cs
@@ -60,7 +60,7 @@ namespace Sample.Jockey
 
             Console.ReadLine();
             HorseClient client = new();
-            await client.ConnectAsync("horse://localhost:26222");
+            await client.ConnectAsync("horse://localhost:15400");
 
             var f = new {ok = true, id = Guid.NewGuid().ToString(), date = 312783621786, extra = new {a = 23, b = "foo"}};
             await client.Queue.PushJson("DemoQueue2", f, false);
@@ -69,7 +69,7 @@ namespace Sample.Jockey
             Console.ReadLine();
 
             HorseClient consumer = new();
-            await consumer.ConnectAsync("horse://localhost:26222");
+            await consumer.ConnectAsync("horse://localhost:15400");
             consumer.AutoAcknowledge = true;
             await consumer.Queue.Subscribe("DemoQueue2", true);
 
